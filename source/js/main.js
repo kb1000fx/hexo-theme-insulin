1199799198b668fc333d4921643b59b777f65838
kb1000 <724160351@qq.com>
2020-06-01 20:30:38 +0800

    update 6.1

--- source/js/main.js
+++ source/js/main.js
@@ -16,7 +16,9 @@ var app = new Vue({
       changyanClicked: false,
       disqusClicked: false,
       valineClicked: false,
-      searchFieldValue: null,
+      searchHeaderValue: null,
+      searchPageValue: null,
+      searchData: new Array(),
     }
   },
 
@@ -48,9 +50,43 @@ var app = new Vue({
         );
       }
     },
-    EnterSearch: function(){
-      console.log("111");
-      window.location.href = "/search/?"+ this.searchFieldValue
+    EnterSearch: function(varSearch,isLocal){
+      if(isLocal){
+        window.location.href = "/search/?" + encodeURI(varSearch); 
+      } else {
+        window.location.href = "https://www.google.com/search?q=" + encodeURI(varSearch) + " site:" + window.location.hostname;
+      };
+    },   
+    Search: function(varStr){
+      keywords=varStr.split(/\s+/);
+      var str="";
+      for (const data of this.searchData) {
+        var data_title = data.title;
+        var data_url = data.url;
+        var data_content = data.content.trim().replace(/<[^>]+>/g, '').toLowerCase();
+        var index_content = -1;
+        if (data_content !== '') {
+          for (const keyword of keywords) {
+            if(index_content==-1){
+              index_content = data_content.indexOf(keyword);
+            }
+          };
+        };
+        if(index_content!=-1){
+          var start = index_content - 20;
+          var end = index_content + 80;
+          if(start < 0){start = 0;};
+          if(end > data_content.length){end = data_content.length;};
+          str += "<div class='search-post'><a class='search-post-title' href='" + data_url + "'>" + data_title + "</a><p class='search-post-content'>" + data_content.substr(start,end) + "</p><hr></div>";
+        };
+      };
+      for (const keyword of keywords) {
+        str=str.replace(eval('/'+keyword+'/g'),"<span class='search-post-bold'>" + keyword + "</span>")
+      };
+      if(str.length==0){
+        str="<div class='search-post'>暂无</div>";
+      };
+      document.getElementById("search-result").innerHTML=str;
     },
   },
 
@@ -68,6 +104,23 @@ var app = new Vue({
     if (localStorage.getItem('insulin-dark')=='true'){
       this.$vuetify.theme.dark = true;
       this.nightMode = "brightness_2";
+    };
+    //初始化搜索
+    var xmlHttp=new XMLHttpRequest();
+    xmlHttp.open("GET",'/search.xml',false);
+    xmlHttp.send();
+    var xmlResource=xmlHttp.responseXML.getElementsByTagName("entry");
+    for (const post of xmlResource) {
+      this.searchData.push({
+        title: post.getElementsByTagName("title")[0].childNodes[0].nodeValue,
+        content: post.getElementsByTagName("content")[0].childNodes[0].nodeValue,
+        url: post.getElementsByTagName("url")[0].childNodes[0].nodeValue,
+      });
+    }
+    //搜索
+    if(location.pathname=='/search/'){
+      this.searchPageValue=decodeURI(location.search.substr(1));
+      this.Search(this.searchPageValue);
     };
   },
 })
