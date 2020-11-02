610106261eb0c99854ae8de539949f6164f6a92a
kb1000 <724160351@qq.com>
2020-11-02 16:07:10 +0800

    add ad

--- ad.js
+++ ad.js
@@ -5,14 +5,42 @@
         script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
         document.body.appendChild(script);
     };
+
+    function adStr(id, adClient, adSlot) {
+        var str = 
+            '<div id="'+ id +'"><ins class="adsbygoogle"'+
+                'style="display:block"'+
+                'data-ad-client="' + adClient + '"'+
+                'data-ad-slot="' + adSlot + '"'+
+                'data-ad-format="auto"'+
+                'data-full-width-responsive="true">'+
+            '</ins></div>';
+        return str
+    }
     
     function main(hook) {
-        if (!$docsify.ga) {
+        if(!$docsify.adClient) {
           console.error('[Docsify] ga is required.');
           return;
+        }else{
+            hook.beforeEach(appendScript);
+            if($docsify.adHeaderSlot){
+                hook.beforeEach(function(content){
+                    return adStr('header-ad', $docsify.adClient, $docsify.adHeaderSlot) + "\n\n" + content
+                });
+                hook.ready(function() {
+                    (adsbygoogle = window.adsbygoogle || []).push({});
+                });
+            }
+            if($docsify.adFooterSlot){
+                hook.beforeEach(function(content){
+                    return content + "\n\n" + adStr('footer-ad', $docsify.adClient, $docsify.adFooterSlot) 
+                });
+                hook.ready(function() {
+                    (adsbygoogle = window.adsbygoogle || []).push({});
+                });            
+            }
         }
-    
-        hook.beforeEach(collect);
     };
     
     $docsify.plugins = [].concat(main, $docsify.plugins);
